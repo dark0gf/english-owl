0aeeb1873a992624c3a6c48724c52721d3dee258
dark0gf <dark0gf@gmail.com>
2019-09-20 12:39:45 +0300

    add comments for intrafaces

--- src/app/pages/watch/interfaces.ts
+++ src/app/pages/watch/interfaces.ts
@@ -2,7 +2,16 @@ import PlayerState from 'youtube-player/dist/constants/PlayerStates';
 import { EventType } from 'youtube-player/dist/eventNames';
 
 export interface IVideoData {
+  /**
+   * youtube player will start playing from that time
+   * seconds
+   */
   startFrom: number,
+  /**
+   * english subtitles that can be clicked and translated
+   * s and e - start and end seconds when to show subtitle
+   * t - text of subtitle
+   */
   textData: Array<{s: number, e: number, t: string}>
 }
 
@@ -16,6 +25,10 @@ export interface IPage {
   loading: boolean,
 }
 
+/**
+ * Need this one for YouTubePlayer because types from package have not good typings
+ * (ex: getCurrentTime() - returns Promise<number>, but in types it appears as number)
+ */
 export interface YouTubePlayer {
   addEventListener(event: string, listener: (event: CustomEvent) => void): void;
   destroy(): void;
